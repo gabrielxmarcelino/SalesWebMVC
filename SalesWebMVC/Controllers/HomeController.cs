33356cd8b398f56691cfa090f315906aac504e69
xMarcelino <gabriel.marcelinob15@gmail.com>
2023-01-10 08:16:03 -0300

    First Model-Controller-View - Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
