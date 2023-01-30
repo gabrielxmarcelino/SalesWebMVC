13d61843e21ffd8dc10906f0cc71f251b777a6f6
xMarcelino <gabriel.marcelinob15@gmail.com>
2023-01-30 07:23:41 -0300

    Validation

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -29,15 +29,22 @@ namespace SalesWebMVC.Controllers
 		}
 		public IActionResult Create()
 		{
+			
 			var departments = _departmentService.FindAll();
 			var viewModel = new SellerFormViewModel { Departments = departments };
 			return View(viewModel);
 		}
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult Create(Seller Seller)
+		public IActionResult Create(Seller seller)
 		{
-			_sellerService.Insert(Seller);
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewModel);
+			}
+			_sellerService.Insert(seller);
 			return RedirectToAction(nameof(Index));
 		}
 
@@ -98,6 +105,12 @@ namespace SalesWebMVC.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Edit(int id, Seller seller)
 		{
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewModel);
+			}
 			if (id != seller.Id)
 			{
 				return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
